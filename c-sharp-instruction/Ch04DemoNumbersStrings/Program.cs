139382ed22e6b6be232243fb63a47824067a5c26
jhawes011 <jhawes011@gmail.com>
2025-01-31 12:05:44 -0500

    Init Commit CsharpNumbersAndStrings

---
+++ c-sharp-instruction/Ch04DemoNumbersStrings/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ch04DemoNumbersStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Numbers and Strings Demo");
+            int dayofweek = 5;
+            decimal price = 11.99m;
+            int quantity = 3;
+            char letter = 'A';
+            bool isValid = false;
+            double interestRate = .0367;
+
+            // constants
+            const int DaysOfTheWeek = 7;
+            const int DaysInNovember = 30;
+            const int DAYS_IN_YEAR = 365;
+
+            // arithmatic operators
+            decimal LineItemTotal = price * quantity;
+            int count = 0;
+            count = count + 1;
+            Console.WriteLine(count);
+
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
---
+++ c-sharp-instruction/HelloWorld2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Happy Friday!!! Woo Hoo!!");
+            Console.WriteLine('X');
+            Console.WriteLine(45);
+            Console.WriteLine(11.11);
+        }
+    }
+}
