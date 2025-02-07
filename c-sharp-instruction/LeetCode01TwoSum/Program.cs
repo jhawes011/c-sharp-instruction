98acc3463f7e84855ad0225af538a539aec17fd0
jhawes011 <jhawes011@gmail.com>
2025-02-07 16:39:09 -0500

    init commit LeetCode TwoSum

--- c-sharp-instruction/LeetCode01TwoSum/Program.cs
+++ c-sharp-instruction/LeetCode01TwoSum/Program.cs
@@ -5,6 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+			// example 1-------------------------------------------
+			int[] nums = { 2, 7, 11, 15 };
+			int target = 9;
+
+			int[] indices = TwoSum(nums, target);
+			Console.WriteLine("indices: ");
+			foreach (int i in indices)
+			{ Console.WriteLine($"indices: {indices[i]}"); }
+
+
+
         }
+
+
+
+
+		static int[] TwoSum(int[] nums, int target)
+		{
+			int[] indices = new int[2];
+			return indices;
+		}
     }
 }
