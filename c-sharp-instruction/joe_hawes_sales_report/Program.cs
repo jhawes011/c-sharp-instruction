2cfba32b32ef86f3ff33f19002aa1e49c4195d5b
jhawes011 <jhawes011@gmail.com>
2025-02-07 09:24:33 -0500

    init commit sales report project

--- c-sharp-instruction/joe_hawes_sales_report/Program.cs
+++ c-sharp-instruction/joe_hawes_sales_report/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace joe_hawes_sales_report
+﻿using System.Data;
+using System.Globalization;
+
+namespace joe_hawes_sales_report
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("The Sales Report Application \n ------------------------------------------------------");
+			
+
+			Console.WriteLine("Goodbye-------------------------------------------------");
+		}
+	}
 }
