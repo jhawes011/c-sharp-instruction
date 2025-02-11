6e7292487819e5cdbbb53d5e333276a95ba03a16
jhawes011 <jhawes011@gmail.com>
2025-02-11 10:47:21 -0500

    init commit for ConsoleLibrary and DiceRoller

---
+++ c-sharp-instruction/joe_hawes_dice_roller2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace joe_hawes_dice_roller2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
