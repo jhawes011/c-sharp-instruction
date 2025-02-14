75885d53fb2c9b4fb0a878799a13196c6373def2
jhawes011 <jhawes011@gmail.com>
2025-02-14 11:34:44 -0500

    init commit ch17 ClassDemo WorkingWithFiles

---
+++ c-sharp-instruction/Ch17ClassDemoWorkingWithFiles/Ch17ClassDemoWorkingWithFiles/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ch17ClassDemoWorkingWithFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====================Ch17 Class Demo - How to work with files=================");
+
+
+
+
+
+
+        }
+    }
+}
