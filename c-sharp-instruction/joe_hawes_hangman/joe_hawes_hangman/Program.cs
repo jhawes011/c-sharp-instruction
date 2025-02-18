a9c9818d67c60a4cacf957d899cf66598d25353e
jhawes011 <jhawes011@gmail.com>
2025-02-18 11:04:28 -0500

    string literal iterations updated

--- c-sharp-instruction/joe_hawes_hangman/joe_hawes_hangman/Program.cs
+++ c-sharp-instruction/joe_hawes_hangman/joe_hawes_hangman/Program.cs
@@ -6,82 +6,99 @@ namespace joe_hawes_hangman
 		static void Main(string[] args)
 		{
 			MyConsole.PrintL("MyConsole.Print($\"\\n==================== Welcome to the Hangman game! ====================\n");
-			Console.WriteLine("Testing DisplayHangman method:\n");
-			int[] testCases = { 0, 1, 2, 3, 4, 5, 6, 7 };
+			
+			MyConsole.PrintL("\n==================== Thanks for playing! =========================");
+		}
 
+		public static string GetHangmanImage(int incorrectGuesses)
+		{
+			switch (incorrectGuesses)
+			{
+				case 6:
+					return
+						"""
+						_____ 
+						|   | D
+						|   O  E
+						|  /|\  A
+						|  / \   D
+						|______   
+						""";
+				case 5:
+					return
+						"""
+						_____ 
+						|   | 
+						|   O 
+						|  /|\
+						|  / 
+						|______
+						""";
+				case 4:
+					return
+						"""
+						_____ 
+						|   | 
+						|   O 
+						|  /|\
+						|  
+						|______
+						""";
+				case 3:
+					return
+						"""
+						_____ 
+						|   | 
+						|   O 
+						|  /|
+						|  
+						|______
+						""";
+				case 2:
+					return
+						"""
+						_____ 
+						|   | 
+						|   O 
+						|   |
+						|  
+						|______
+						""";
+				case 1:
+					return
+						"""
+						_____ 
+						|   | 
+						|   O 
+						|  
+						|  
+						|______
+						""";
+				case 0:
+					return
+						"""
+						_____ 
+						|   | 
+						|
+						|
+						|
+						|______
+						""";
+				default:
+					return string.Empty;
+			}
+
+		}
+		private static void HangmanImageTest()
+		{
+			Console.WriteLine("Testing DisplayHangman method:\n");
+			int[] testCases = { 0, 1, 2, 3, 4, 5, 6 };
 			foreach (int guesses in testCases)
 			{
 				Console.WriteLine($"Incorrect Guesses: {guesses}");
 				Console.WriteLine(GetHangmanImage(guesses));
 				Console.WriteLine();
 			}
-			MyConsole.PrintL("\n==================== Thanks for playing! =========================");
 		}
-		public static string GetHangmanImage(int incorrectGuesses)
-		{
-			switch (incorrectGuesses)
-			{
-				case 0:
-					return
-						"-----  \r\n" +
-						"|   |  \r\n" +
-						"|   O  \r\n" +
-						"|  /|\\\r\n" +
-						"|  / \\\r\n" +
-						"|\r\n-------\r\n";
-				case 1:
-					return
-						"-----  \r\n" +
-						"|   |  \r\n" +
-						"|   O  \r\n" +
-						"|  /|\\\r\n" +
-						"|    \\\r\n" +
-						"|\r\n-------\r\n";
-				case 2:
-					return
-						"-----  \r\n" +
-						"|   |  \r\n" +
-						"|   O  \r\n" +
-						"|  /|\\\r\n" +
-						"|      \r\n" +
-						"|\r\n-------\r\n";
-				case 3:
-					return
-						"-----  \r\n" +
-						"|   |  \r\n" +
-						"|   O  \r\n" +
-						"|   |\\\r\n" +
-						"|      \r\n" +
-						"|\r\n-------\r\n";
-				case 4:
-					return
-						"-----  \r\n" +
-						"|   |  \r\n" +
-						"|   O  \r\n" +
-						"|   |  \r\n" +
-						"|      \r\n" +
-						"|\r\n-------\r\n";
-				case 5:
-					return
-						"-----   \r\n" +
-						"|   |   \r\n" +
-						"|   O   \r\n" +
-						"|       \r\n" +
-						"|       \r\n" +
-						"|\r\n-------\r\n";
-				case 6:
-					return
-						"-----  \r\n" +
-						"|   D  \r\n" +
-						"|   E  \r\n" +
-						"|   A  \r\n" +
-						"|   D  \r\n" +
-						"|\r\n-------\r\n";
-				default:
-					return string.Empty;
-			}
-
-		}
-
 	}
 }
