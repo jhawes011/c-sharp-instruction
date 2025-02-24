69235f5d5ac6dfd74f368f12661bc965659cbd25
Joe Hawes <jhawes011@gmail.com>
2025-02-24 11:03:18 -0500

    Class Demo Names Array

---
+++ ClassDemoStudentNamesArray/ClassDemoStudentNamesArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleLibrary;
+namespace ClassDemoStudentNamesArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Student Array Demo");
+
+			int nbrStudents = MyConsole.PromptInt("Number of students: ");
+			string[] studentsArray = new string[nbrStudents];
+
+			for (int i = 0; i < nbrStudents; i++)
+			{
+				string name = MyConsole.PromptString($"Student Name[{i}]: ");
+				studentsArray[i] = name;
+			}
+
+			string names = "";
+			foreach (string name in studentsArray)
+			{
+				names += name + "|";
+			}
+			MyConsole.PrintL(names);
+
+
+
+        }
+
+
+
+
+
+    }
+}
